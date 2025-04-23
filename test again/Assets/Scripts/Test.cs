152ba8ab198a78ba98797ba98b90b169daefc570
PartyPandaStar <scoondi@hotmail.com>
2025-04-23 15:38:14 +0200

    hghjk

--- test again/Assets/Scripts/Test.cs
+++ test again/Assets/Scripts/Test.cs
@@ -5,8 +5,9 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int number = 1;
+        int number = 5000;
         Debug.Log(number);
+        //this is a comment
     }
 
     // Update is called once per frame

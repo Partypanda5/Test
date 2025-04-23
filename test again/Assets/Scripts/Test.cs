e0f54a7837699ba18f25b4ebf3b0597a74a02904
PartyPandaStar <scoondi@hotmail.com>
2025-04-23 15:38:59 +0200

    chgfhjgfhg

--- test again/Assets/Scripts/Test.cs
+++ test again/Assets/Scripts/Test.cs
@@ -5,8 +5,9 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int number = 1;
+        int number = 67889;
         Debug.Log(number);
+        //hgjhgfhjfjhgfhjfhg
     }
 
     // Update is called once per frame
